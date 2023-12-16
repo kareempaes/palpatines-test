bd85b7e8ade656736f1a82326db416aa97a2d8d5
kareem paes <contact@kareempaes.com>
2023-12-16 11:19:17 -0700

    add awilix setup and register any modules in core

---
+++ server/cmd/http/DI/awilix.ts
@@ -0,0 +1,14 @@
+import { createContainer } from 'awilix';
+
+// Create container
+const container = createContainer();
+
+// Load modules
+const modules = container.loadModules([
+  '../../config/*.config.ts',
+  '../../core/service/*.service.ts',
+  '../../core/repository/*.repository.ts',
+  '../../core/use-case/*.use-case.ts',
+]);
+
+export default container;
