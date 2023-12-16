0b2e2cb5edd4cfab4497ab9fd786a2ef754fc2fa
kareem paes <contact@kareempaes.com>
2023-12-16 11:00:39 -0700

    add server configuration

---
+++ server/config/server.ts
@@ -0,0 +1,23 @@
+interface Server {
+  start: () => void;
+}
+
+const server = ({ app }) => {
+
+  app.get('/_health', (req, res) => {
+    res.send('OK');
+  });
+
+  const start = () => {
+    app.listen(3000, () => {
+      console.log('Server listening on port 3000');
+    });
+  };
+
+  return {
+    start,
+  };
+}
+
+export default server;
+
