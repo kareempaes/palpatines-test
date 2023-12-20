3cfafd5118c3ef7c8625164921568a46bc20fab0
kareem paes <contact@kareempaes.com>
2023-12-20 09:23:51 -0700

    remove consoles

--- server/core/service/swapi.service.ts
+++ server/core/service/swapi.service.ts
@@ -12,15 +12,10 @@ export interface SwapiServiceOptions {
 
 const swapiService = (opts: SwapiServiceOptions): SwapiService => {
 
-  const getHomeWorld = async (url: string) => {
-    
-    
+  const getHomeWorld = async (url: string) => {    
     const response = await opts.axiosConfig.clients[ClientConstant.SWAPI].get(url);
-
-    console.log(response.data);
-    return response.data.results;
-
     
+    return response.data.results;
   }
 
 
